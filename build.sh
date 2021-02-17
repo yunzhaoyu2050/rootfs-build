53bce9269f160e59109f7a9551b8e0853deb0562
yunzhaoyu2050 <yunzhaoyu@outlook.com>
2021-02-17 09:23:04 +0800

    update: build.sh

--- build.sh
+++ build.sh
@@ -9,24 +9,21 @@ echo -e "\033[32m Config :system_type:$RF_SYSTEM_TYPE, system_version:$RF_SYSTEM
 
 echo -e "\033[32m Download source rootfs.\033[0m"
 ./download_rootfs.sh
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
     echo -e "\033[31m download_rootfs.sh failed.\033[0m"
     exit 1
 fi
 
 echo -e "\033[32m Create rootfs.\033[0m"
 ./create-rootfs.sh
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
     echo -e "\033[31m create-rootfs.sh failed.\033[0m"
     exit 1
 fi
 
 echo -e "\033[32m Create image.\033[0m"
 ./create-image.sh
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
     echo -e "\033[31m create-image.sh failed.\033[0m"
     exit 1
 fi
